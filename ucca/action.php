dca76efb86f3ddf82a7a8f8adcf5c6fb200e43d0
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-09-13 16:13:54 +0300

    Refactor PHP

--- ucca/action.php
+++ ucca/action.php
@@ -1,17 +1,25 @@
 <?php
 $server = file_get_contents("server_name.txt");
-if(isset($_POST["action"])){
+if (isset($_POST["action"])) {
     $action = $_POST["action"];
-    if($action == "parse"){
+    if ($action == "parse") {
+        header("Content-type: text/plain; charset=utf-8");
         $data = http_build_query(array("input" => $_POST["input"]));
-    }elseif($action == "download"){
-        $data = http_build_query(array("input" => $_POST["xml"], "format" => $_POST["format"]));
+    } elseif ($action == "download") {
+        $format = $_POST["format"];
+        header('Content-description: File Transfer');
+        header("Content-disposition: attachment; filename=\"out.$format\"");
+        header("Content-type: application/octet-stream");
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        $action = "parse";
+        $data = http_build_query(array("input" => $_POST["xml"], "format" => $format));
     }
-}else{
+} else {
     $action = "visualize";
     $data = file_get_contents('php://input');
 }
-
 $options = array(
     "http" => array(
         "header"  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -20,17 +28,5 @@ $options = array(
     )
 );
 $context = stream_context_create($options);
-if($action == "download"){
-    $format = $_POST["format"];
-    header('Content-description: File Transfer');
-    header("Content-disposition: attachment; filename=\"out.$format\"");
-    header("Content-type: application/octet-stream");
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    $action = "parse";
-}else{
-    header("Content-type: text/plain; charset=utf-8");
-}
 echo file_get_contents("http://$server/$action", false, $context);
 ?>
