ed929089ca9585d80732b5992cb504ca6b43e075
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-09-13 12:33:56 +0300

    Add download button (not working yet, hidden); simplify

--- ucca/parse.php
+++ ucca/parse.php
@@ -1,18 +1,22 @@
 <?php
 $server = file_get_contents("server_name.txt");
-$url = "http://$server/parse";
-$data = array("input" => $_POST["input"]);
+$url = "http://$server/";
+if(isset($_POST["input"])){
+    $url .= "parse";
+    $data = http_build_query(array("input" => $_POST["input"]));
+}else{
+    $url .= "visualize";
+    $data = file_get_contents('php://input');
+}
 
 $options = array(
     "http" => array(
         "header"  => "Content-type: application/x-www-form-urlencoded\r\n",
         "method"  => "POST",
-        "content" => http_build_query($data)
+        "content" => $data
     )
 );
 $context = stream_context_create($options);
-header('Content-type: application/xml; charset=utf-8');
-$xml = simplexml_load_string(file_get_contents($url, false, $context));
-$dom = dom_import_simplexml($xml);
-echo $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+header("Content-type: text/plain; charset=utf-8");
+echo file_get_contents($url, false, $context);
 ?>
