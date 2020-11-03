a5ca00302b6cfaa97b3c17f618da8a4a7e7c6a02
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-11-03 16:38:07 +0100

    Install spaCy model before running

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ export PARSER_MODEL=models/$MODEL
 export PARSER_TYPE=sparse
 export IP=0.0.0.0
 echo localhost:5001 >server_name.txt
+python -m spacy download $SPACY_MODEL
 until python parse_server.py -vv; do
   echo Re-running server...
   sleep 10
